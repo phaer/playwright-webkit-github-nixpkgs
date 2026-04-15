d550cb84cdee9c388a39347ee2b717e51b1ec862
Paul Haerle <hello@phaer.org>
2026-04-16 00:02:11 +0200

    Set LIBGL_ALWAYS_SOFTWARE environment variable

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
             PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
             PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
             PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = 1;
+            LIBGL_ALWAYS_SOFTWARE=1;
           };
         }
       );
