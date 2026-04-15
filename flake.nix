01fa00c4fc0261462ddc2f5dd89a078f52a1578c
Alan Norbauer <alan@norbauer.com>
2026-04-15 13:42:29 -0700

    top-level env vars

--- flake.nix
+++ flake.nix
@@ -38,11 +38,9 @@
               pkgs.playwright-test
               pkgs.playwright-driver.browsers
             ];
-            shellHook = ''
-              export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
-              export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-              export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=1
-            '';
+            PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+            PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
+            PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = 1;
           };
         }
       );
