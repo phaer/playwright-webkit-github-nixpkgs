dad25e46ea46cc9375700fa75b27bb06cbd5f241
Paul Haerle <hello@phaer.org>
2026-04-16 00:03:14 +0200

    Rename test case to include 'Foobar'

--- tests/hello.spec.ts
+++ tests/hello.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("hello is visible", async ({ page }) => {
+test("hello is visible. Foobar", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByText("hello")).toBeVisible();
 });
